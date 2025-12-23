19a04bc7514ec3114b0549de02576211d890ffce
biarmortari <beatrizrmortari@gmail.com>
2025-12-23 14:19:50 -0300

    fix(): configra porta para deploy

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Conectado ao MongoDB de Produção/Dev");
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`O app está executando na porta ${PORT}`);
     });
   })
